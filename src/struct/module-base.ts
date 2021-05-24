78e796f1fefb078a3b538204e8485997c05102d2
Daison Carino <daison12006013@gmail.com>
2021-05-24 23:25:24 +0800

    Update structs

--- src/struct/module-base.ts
+++ src/struct/module-base.ts
@@ -1,4 +1,15 @@
-import type { User as UserContract } from '$struct/_contracts'
+/**
+ * Under this module-base: we should only put every interfaces/constants/enums that is related to the
+ * "module-base" itself of lahatan.com
+ */
+import type { Default } from '$struct/_contracts'
+
+interface UserContract extends Default {
+  id: number
+  name: string
+  email?: string
+  email_verified_at?: string | null
+}
 
 export const User = (user: UserContract) => {
   // maybe you want to add more validations here?
--- src/struct/module-property.ts
+++ src/struct/module-property.ts
@@ -1,5 +1,28 @@
+/**
+ * Under this module-property: we should only put every interfaces/constants/enums that is related to the
+ * "module-property" itself of lahatan.com
+ */
 import { COUNTRIES } from '$struct/_constants'
-import type { Post as PostContract } from '$struct/_contracts'
+import type { Default } from '$struct/_contracts'
+
+interface PostContract extends Default {
+  tenant: string
+  title: string
+  uuid: string
+  description: string
+  category: string
+  address: string
+  postal_code: string
+  latitude: number | null
+  longitude: number | null
+  type: string
+  status: string
+  number_of_rooms: number
+  number_of_toilets: number
+  area_size: number
+  price: number
+  expires_at: string
+}
 
 export const Post = (post: PostContract) => {
   if (!COUNTRIES[post.tenant].alias) {
