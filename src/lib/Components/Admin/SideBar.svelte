00af8c2b7c3a756b4641c54f134065ebc02b5aaf
Daison Carino <daison12006013@gmail.com>
2021-06-06 00:56:07 +0800

    Fix sidebar menus

--- src/lib/Components/Admin/SideBar.svelte
+++ src/lib/Components/Admin/SideBar.svelte
@@ -1,238 +1,155 @@
 <script lang="ts">
-  import { isPagesMenuOpen } from '$stores/menus'
+  import { pageMenus, togglePageMenu } from '$stores/menus'
+  import { page } from '$app/stores'
+
+  const links = [
+    {
+      name: 'Dashboard',
+      url: '/',
+      svg: [
+        'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6',
+      ],
+    },
+    {
+      name: 'Forms',
+      url: '/forms',
+      svg: [
+        'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01',
+      ],
+    },
+    {
+      name: 'Cards',
+      url: '/cards',
+      svg: [
+        'M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10',
+      ],
+    },
+    {
+      name: 'Charts',
+      url: '/charts',
+      svg: [
+        'M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z',
+        'M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z',
+      ],
+    },
+    {
+      name: 'Buttons',
+      url: '/buttons',
+      svg: [
+        'M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122',
+      ],
+    },
+    {
+      name: 'Modals',
+      url: '/modals',
+      svg: [
+        'M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z',
+      ],
+    },
+    { name: 'Tables', url: '/tables', svg: ['M4 6h16M4 10h16M4 14h16M4 18h16'] },
+    {
+      name: 'Pages',
+      svg: [
+        'M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z',
+      ],
+      sublinks: [
+        { name: 'Login', url: '/' },
+        { name: 'Create account', url: '/' },
+        { name: 'Forgot password', url: '/' },
+        { name: '404', url: '/' },
+        { name: 'Blank', url: '/' },
+      ],
+    },
+  ]
+
+  const matchesUrlPath = (val: string) => {
+    return val === $page.path
+  }
 </script>
 
 <div class="py-4 text-gray-500 dark:text-gray-400">
-  <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#"> Windmill </a>
+  <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="/"> Windmill </a>
   <ul class="mt-6">
-    <li class="relative px-6 py-3">
-      <span
-        class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-        aria-hidden="true"
-      />
-      <a
-        class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-        href="index.html"
-      >
-        <svg
-          class="w-5 h-5"
-          aria-hidden="true"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
-        <span class="ml-4">Dashboard</span>
-      </a>
-    </li>
-  </ul>
-  <ul>
-    <li class="relative px-6 py-3">
-      <a
-        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-        href="forms.html"
-      >
-        <svg
-          class="w-5 h-5"
-          aria-hidden="true"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
-          />
-        </svg>
-        <span class="ml-4">Forms</span>
-      </a>
-    </li>
-    <li class="relative px-6 py-3">
-      <a
-        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-        href="cards.html"
-      >
-        <svg
-          class="w-5 h-5"
-          aria-hidden="true"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-          />
-        </svg>
-        <span class="ml-4">Cards</span>
-      </a>
-    </li>
-    <li class="relative px-6 py-3">
-      <a
-        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-        href="charts.html"
-      >
-        <svg
-          class="w-5 h-5"
-          aria-hidden="true"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
-          <path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
-        </svg>
-        <span class="ml-4">Charts</span>
-      </a>
-    </li>
-    <li class="relative px-6 py-3">
-      <a
-        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-        href="buttons.html"
-      >
-        <svg
-          class="w-5 h-5"
-          aria-hidden="true"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
-          />
-        </svg>
-        <span class="ml-4">Buttons</span>
-      </a>
-    </li>
-    <li class="relative px-6 py-3">
-      <a
-        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-        href="modals.html"
-      >
-        <svg
-          class="w-5 h-5"
-          aria-hidden="true"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-          />
-        </svg>
-        <span class="ml-4">Modals</span>
-      </a>
-    </li>
-    <li class="relative px-6 py-3">
-      <a
-        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-        href="tables.html"
-      >
-        <svg
-          class="w-5 h-5"
-          aria-hidden="true"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M4 6h16M4 10h16M4 14h16M4 18h16" />
-        </svg>
-        <span class="ml-4">Tables</span>
-      </a>
-    </li>
-    <li class="relative px-6 py-3">
-      <button
-        class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-        aria-haspopup="true"
-      >
-        <span class="inline-flex items-center">
-          <svg
-            class="w-5 h-5"
+    {#each links as link, a}
+      <li class="relative px-6 py-3">
+        {#if matchesUrlPath(link.url)}
+          <span
+            class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
             aria-hidden="true"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
-            />
-          </svg>
-          <span class="ml-4">Pages</span>
-        </span>
-        <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-          <path
-            fill-rule="evenodd"
-            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            clip-rule="evenodd"
           />
-        </svg>
-      </button>
-      {#if $isPagesMenuOpen}
-        <template>
-          <ul
-            x-transition:enter="transition-all ease-in-out duration-300"
-            x-transition:enter-start="opacity-25 max-h-0"
-            x-transition:enter-end="opacity-100 max-h-xl"
-            x-transition:leave="transition-all ease-in-out duration-300"
-            x-transition:leave-start="opacity-100 max-h-xl"
-            x-transition:leave-end="opacity-0 max-h-0"
-            class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
-            aria-label="submenu"
+        {/if}
+
+        {#if !link.sublinks}
+          <a
+            class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+            class:text-gray-800={matchesUrlPath(link.url)}
+            class:dark:text-gray-100={matchesUrlPath(link.url)}
+            href={link.url}
           >
-            <li
-              class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+            <svg
+              class="w-5 h-5"
+              aria-hidden="true"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <a class="w-full" href="pages/login.html">Login</a>
-            </li>
-            <li
-              class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+              {#each link.svg as s, b}
+                <path d={s} />
+              {/each}
+            </svg>
+            <span class="ml-4">{link.name}</span>
+          </a>
+        {:else}
+          <button
+            on:click={() => togglePageMenu(link.name)}
+            class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+            aria-haspopup="true"
+          >
+            <span class="inline-flex items-center">
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
+                />
+              </svg>
+              <span class="ml-4">{link.name}</span>
+            </span>
+            <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+          {#if $pageMenus[link.name]}
+            <ul
+              class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
+              aria-label="submenu"
             >
-              <a class="w-full" href="pages/create-account.html"> Create account </a>
-            </li>
-            <li
-              class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-            >
-              <a class="w-full" href="pages/forgot-password.html"> Forgot password </a>
-            </li>
-            <li
-              class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-            >
-              <a class="w-full" href="pages/404.html">404</a>
-            </li>
-            <li
-              class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-            >
-              <a class="w-full" href="pages/blank.html">Blank</a>
-            </li>
-          </ul>
-        </template>
-      {/if}
-    </li>
+              {#each link.sublinks as sublink, c}
+                <li
+                  class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                >
+                  <a class="w-full" href={sublink.url}>{sublink.name}</a>
+                </li>
+              {/each}
+            </ul>
+          {/if}
+        {/if}
+      </li>
+    {/each}
   </ul>
   <div class="px-6 my-6">
     <button
--- src/stores/menus.ts
+++ src/stores/menus.ts
@@ -1,10 +1,22 @@
 import { writable } from 'svelte/store'
 
 export const isDark = writable(false)
-export const isPagesMenuOpen = writable(false)
 export const isSideMenuOpen = writable(false)
 export const isNotificationsMenuOpen = writable(false)
 export const isProfileMenuOpen = writable(false)
+export const pageMenus = writable([])
+
+export const togglePageMenu = (name: string) => {
+  pageMenus.update((pages) => {
+    if (typeof pages[name] === 'undefined') {
+      pages[name] = true
+    } else {
+      pages[name] = !pages[name]
+    }
+
+    return pages
+  })
+}
 
 export const toggleTheme = () => {
   window.document.getElementById('body').classList.toggle('dark')
