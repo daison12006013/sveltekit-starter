96ac18bf816c151056f9709719e6fafef02ad91b
Daison Carino <daison12006013@gmail.com>
2021-06-07 02:10:43 +0800

    Fix cancel button

--- src/routes/modals.svelte
+++ src/routes/modals.svelte
@@ -64,7 +64,7 @@
     >
       <button
         on:click={closeModal}
-        class="w-full px-5 py-3 text-sm font-medium leading-5 text-white text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray"
+        class="w-full px-5 py-3 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray"
       >
         Cancel
       </button>
