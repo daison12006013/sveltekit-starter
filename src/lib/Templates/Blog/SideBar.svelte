b105bd624daa18a1777c77043f45552a4334088d
Daison Carino <daison12006013@gmail.com>
2021-06-17 06:24:06 +0800

    Change sidebar color

--- src/lib/Templates/Blog/SideBar.svelte
+++ src/lib/Templates/Blog/SideBar.svelte
@@ -32,14 +32,14 @@
       <li class="relative px-6 py-3">
         {#if activeMenu == link.url}
           <span
-            class="absolute inset-y-0 left-0 w-2 bg-red-500 rounded-tr-lg rounded-br-lg"
+            class="absolute inset-y-0 left-0 w-2 bg-blue-500 rounded-tr-lg rounded-br-lg"
             aria-hidden="true"
           />
         {/if}
 
         <a
           class="{activeMenu == link.url &&
-            'text-red-200'} inline-flex items-center w-full text-3xl font-semibold transition-colors duration-150 hover:text-red-500"
+            'text-blue-200'} inline-flex items-center w-full text-3xl font-semibold transition-colors duration-150 hover:text-blue-300"
           href={link.url}
           on:click={(e) => {
             e.preventDefault()
